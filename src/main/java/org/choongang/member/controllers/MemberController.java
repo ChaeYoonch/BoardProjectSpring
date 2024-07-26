9657fc74105fefdd29a7e52793e213ccaf7d1963
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 15:59:00 +0900

    MemberController 파일 : 회원가입 & 프로세스 기초 설정

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -2,6 +2,7 @@ package org.choongang.member.controllers;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
@@ -11,5 +12,10 @@ public class MemberController {
     @GetMapping("/join") // url 주소
     public String join() { // 회원가입
         return "front/member/join";
+    }
+
+    @PostMapping("/join")
+    public String joinPs() { // 회원가입 프로세스
+        return "redirect:/member/login";
     }
 }
