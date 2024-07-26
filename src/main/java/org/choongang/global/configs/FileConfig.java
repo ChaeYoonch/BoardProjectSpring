a3ac71e9f285da84c61763aeac3853f348a39f50
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 15:00:50 +0900

    FileConfig 파일 기초 설정

--- src/main/java/org/choongang/global/configs/FileConfig.java
+++ src/main/java/org/choongang/global/configs/FileConfig.java
@@ -1,9 +1,21 @@
 package org.choongang.global.configs;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class FileConfig implements WebMvcConfigurer {
 
+    @Value("${file.upload.path") // 설정 내용에서 값을 가져와 주입
+    private String path;
+
+    @Value("${file.upload.url") // 설정 내용에서 값을 가져와 주입
+    private String url;
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+
+    }
 }
