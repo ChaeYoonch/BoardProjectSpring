4d2441ab8767c11e2d5e2643d308bcc0a011aecf
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 15:21:05 +0900

    FileConfig 파일 기초 설정

--- src/main/java/org/choongang/global/configs/FileConfig.java
+++ src/main/java/org/choongang/global/configs/FileConfig.java
@@ -8,10 +8,10 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class FileConfig implements WebMvcConfigurer {
     // application.yml 의 # 파일 업로드 경로 설정 부분 연동
-    @Value("${file.upload.path") // 설정 내용에서 값을 가져와 주입
+    @Value("${file.upload.path}") // 설정 내용에서 값을 가져와 주입
     private String path;
 
-    @Value("${file.upload.url") // 설정 내용에서 값을 가져와 주입
+    @Value("${file.upload.url}") // 설정 내용에서 값을 가져와 주입
     private String url;
 
     @Override
