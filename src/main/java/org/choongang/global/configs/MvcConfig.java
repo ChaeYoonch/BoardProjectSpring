f342d10c0ce2cf5c18200070ecd95eefe440af55
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 15:42:14 +0900

    MvcConfig 파일 기초 설정 (@EnableJpaAuditing)

--- src/main/java/org/choongang/global/configs/MvcConfig.java
+++ src/main/java/org/choongang/global/configs/MvcConfig.java
@@ -1,9 +1,11 @@
 package org.choongang.global.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableJpaAuditing // 세팅 기초 설정에 필요
 public class MvcConfig implements WebMvcConfigurer { // MVC 기본 설정
 
 }
