fcf242e82ecb6cf9a94df1a49bfac9daa5e5d481
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 14:53:36 +0900

    SecurityConfig 파일 생성

--- src/main/java/org/choongang/global/configs/SecurityConfig.java
+++ src/main/java/org/choongang/global/configs/SecurityConfig.java
@@ -1,5 +1,15 @@
 package org.choongang.global.configs;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
 public class SecurityConfig {
 
+    @Bean // 스프링 관리 객체로 설정
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+    }
 }
