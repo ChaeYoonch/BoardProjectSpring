c41969e40e41caa04ecbfa45bf1fa88d0733ea20
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 15:46:12 +0900

    MvcConfig 파일 : HiddenHttpMethodFilter 설정

--- src/main/java/org/choongang/global/configs/MvcConfig.java
+++ src/main/java/org/choongang/global/configs/MvcConfig.java
@@ -1,11 +1,21 @@
 package org.choongang.global.configs;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 @EnableJpaAuditing // 세팅 기초 설정에 필요
 public class MvcConfig implements WebMvcConfigurer { // MVC 기본 설정
 
+    /**
+     * <input type="hidden" name="_method" value="PATCH"> -> PATCH 방식으로 요청
+     * @return
+     */
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
+    }
 }
