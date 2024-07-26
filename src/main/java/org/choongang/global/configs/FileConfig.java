3b22b951ccd507c5e84417601d58b1bb0d85ff64
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 15:27:24 +0900

    FileConfig 파일 : @EnableConfigurationProperties(FileProperties.class) 연결

--- src/main/java/org/choongang/global/configs/FileConfig.java
+++ src/main/java/org/choongang/global/configs/FileConfig.java
@@ -1,21 +1,21 @@
 package org.choongang.global.configs;
 
-import org.springframework.beans.factory.annotation.Value;
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
+@EnableConfigurationProperties(FileProperties.class)
+@RequiredArgsConstructor
 public class FileConfig implements WebMvcConfigurer {
-    // application.yml 의 # 파일 업로드 경로 설정 부분 연동
-    @Value("${file.upload.path}") // 설정 내용에서 값을 가져와 주입
-    private String path;
 
-    @Value("${file.upload.url}") // 설정 내용에서 값을 가져와 주입
-    private String url;
+    private final FileProperties properties;
 
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
     }
+
 }
