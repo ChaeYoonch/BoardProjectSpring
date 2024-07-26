38260a011b53d5b043e9de0a737c1623ca1f01ad
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-26 14:57:09 +0900

    MvcConfig 파일 생성

--- src/main/java/org/choongang/global/configs/MvcConfig.java
+++ src/main/java/org/choongang/global/configs/MvcConfig.java
@@ -1,8 +1,9 @@
 package org.choongang.global.configs;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-public class MvcConfig { // MVC 기본 설정
+public class MvcConfig implements WebMvcConfigurer { // MVC 기본 설정
 
 }
