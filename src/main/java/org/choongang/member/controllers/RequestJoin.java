bafa1525fecfea63a497b540877118255ce251c9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 10:48:16 +0900

    RequestJoin : 회원가입

--- src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -4,8 +4,9 @@ import lombok.Data;
 
 @Data
 public class RequestJoin {
+    // 사용자가 작성한 데이터 전달 - 커맨드 객체
     private String email; // 이메일
-    private String password; // 비밀번호 | 사용자가 작성한 데이터 전달 - 커맨드 객체
+    private String password; // 비밀번호
     private String confirmPassword; // 비밀번호 확인
     private String userName; // 회원명
 }
