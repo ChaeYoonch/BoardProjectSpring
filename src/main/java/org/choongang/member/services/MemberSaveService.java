84139f41030246769cee0502773dfa1a3e0d98fc
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 15:59:04 +0900

    MemberSaveService

--- src/main/java/org/choongang/member/services/MemberSaveService.java
+++ src/main/java/org/choongang/member/services/MemberSaveService.java
@@ -6,6 +6,7 @@ import org.choongang.member.entities.Authorities;
 import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.AuthoritiesRepository;
 import org.choongang.member.repositories.MemberRepository;
+import org.modelmapper.ModelMapper;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -25,7 +26,7 @@ public class MemberSaveService {
      * @param form
      */
     public void save(RequestJoin form) {
-
+        Member member = new ModelMapper().map(form, Member.class); // ModelMapper : setter & getter 반복 -> 데이터 치환
     }
 
     public void save(Member member, List<Authorities> authorities) { // 매개 변수 2개
