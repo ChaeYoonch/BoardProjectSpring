93c16465a338f7fbd372897da6da60f40abea6db
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 15:00:27 +0900

    Member

--- src/main/java/org/choongang/member/entities/Member.java
+++ src/main/java/org/choongang/member/entities/Member.java
@@ -1,5 +1,20 @@
 package org.choongang.member.entities;
 
-public class Member {
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.choongang.global.entities.BaseEntity;
 
+@Data
+@Entity
+@Builder
+@NoArgsConstructor @AllArgsConstructor
+public class Member extends BaseEntity {
+    private Long seq;
+    private String email;
+    private String password;
+    private String userName;
+    private String mobile;
 }
