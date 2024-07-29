fa57b178a9c6e4fd07d2f95294aec49d4e4ad32d
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 12:43:50 +0900

    PasswordValidator

--- src/main/java/org/choongang/global/validators/PasswordValidator.java
+++ src/main/java/org/choongang/global/validators/PasswordValidator.java
@@ -1,5 +1,24 @@
 package org.choongang.global.validators;
 
 public interface PasswordValidator {
-    default boolean alphaCheck() // 알파벳 체크
+    /**
+     * 알파벳 복잡성 체크
+     * @param password
+     * @param caseIncensitive - 기본값이 false 가 되면 : 대,소문자 각각 1개 이상 포함 / true : 대,소문자 구분 X
+     * @return
+     */
+    default boolean alphaCheck(String password, boolean caseIncensitive) { // 알파벳 체크
+
+        return false;
+    }
+
+    /**
+     * 숫자 복잡성 체크
+     * @param password
+     * @return
+     */
+    default boolean numberCheck(String password) { // 숫자 체크
+
+        return false;
+    }
 }
