cfe00ac4e933b50c9a3f551d297edd399f6c7998
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:05:08 +0900

    PasswordValidator : 알파벳 / 숫자 / 특수 문자 복잡성 체크

--- src/main/java/org/choongang/global/validators/PasswordValidator.java
+++ src/main/java/org/choongang/global/validators/PasswordValidator.java
@@ -4,10 +4,10 @@ public interface PasswordValidator {
     /**
      * 알파벳 복잡성 체크
      * @param password
-     * @param caseIncensitive - 기본값이 false 가 되면 : 대,소문자 각각 1개 이상 포함 / true : 대,소문자 구분 X
+     * @param caseInsensitive - 기본값이 false 가 되면 : 대,소문자 각각 1개 이상 포함 / true : 대,소문자 구분 X
      * @return
      */
-    default boolean alphaCheck(String password, boolean caseIncensitive) { // 알파벳 체크
+    default boolean alphaCheck(String password, boolean caseInsensitive) { // 알파벳 체크
 
         return false;
     }
