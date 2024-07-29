ef92bdd1501d2dc8934910582527115e7eee43b9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:47:34 +0900

    BaseEntity

--- src/main/java/org/choongang/global/entities/BaseEntity.java
+++ src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -1,9 +1,11 @@
 package org.choongang.global.entities;
 
+import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter @Setter
+@MappedSuperclass
 public abstract class BaseEntity { // 다른 엔티티가 공유할 목적으로 생성
 
 }
