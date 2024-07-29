65c97cceb784784057c0c4131f4b195d3b65f6bd
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:27:35 +0900

    MobileValidator : 전화번호 형식 체크

--- src/main/java/org/choongang/global/validators/MobileValidator.java
+++ src/main/java/org/choongang/global/validators/MobileValidator.java
@@ -1,5 +1,10 @@
 package org.choongang.global.validators;
 
 public interface MobileValidator { // 전화번호
-
+    default boolean checkMobile(String mobile) {
+        /**
+         * 01[016]-0000/000-0000
+         */
+        return false;
+    }
 }
