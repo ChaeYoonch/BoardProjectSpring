10afad527765865ea827866915b0ace0b285774e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:31:25 +0900

    MobileValidator : 전화번호 형식 체크

--- src/main/java/org/choongang/global/validators/MobileValidator.java
+++ src/main/java/org/choongang/global/validators/MobileValidator.java
@@ -4,6 +4,12 @@ public interface MobileValidator { // 전화번호
     default boolean checkMobile(String mobile) {
         /**
          * 01[016]-0000/000-0000
+         * 01[016]-\d{3,4}-\d{4} : 3/4자리 - 4자리
+         * 010.1111.1111
+         * 010-1111-1111
+         * 010 1111 1111
+         * 01011111111
+         * 1. 숫자만 남긴다. 2. 패턴 만들기 3. 체크
          */
         return false;
     }
