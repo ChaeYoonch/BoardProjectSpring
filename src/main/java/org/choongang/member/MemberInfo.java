8f0870b26312d564167475379a48a1ea291fa483
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 16:55:47 +0900

    MemberInfo

--- src/main/java/org/choongang/member/MemberInfo.java
+++ src/main/java/org/choongang/member/MemberInfo.java
@@ -1,5 +1,45 @@
 package org.choongang.member;
 
-public class MemberInfo {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
+import java.util.Collection;
+import java.util.List;
+
+public class MemberInfo implements UserDetails {
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() { // 계정의 잠금 여부
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
