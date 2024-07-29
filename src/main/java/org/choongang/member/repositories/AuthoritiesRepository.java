bc6066f0e451a5d9d5f598645bb62c6cd46ad4c9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 15:33:28 +0900

    AuthoritiesRepository

--- src/main/java/org/choongang/member/repositories/AuthoritiesRepository.java
+++ src/main/java/org/choongang/member/repositories/AuthoritiesRepository.java
@@ -1,5 +1,10 @@
 package org.choongang.member.repositories;
 
-public interface AuthoritiesRepository {
+import org.choongang.member.entities.Authorities;
+import org.choongang.member.entities.AuthoritiesId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface AuthoritiesRepository extends JpaRepository<Authorities, AuthoritiesId>, QuerydslPredicateExecutor<Authorities> {
 
 }
