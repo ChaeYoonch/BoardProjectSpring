8c2f3556c7b414691b8c01403f81dcb4e75e9461
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:49:02 +0900

    BaseEntity

--- src/main/java/org/choongang/global/entities/BaseEntity.java
+++ src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -1,11 +1,14 @@
 package org.choongang.global.entities;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter @Setter
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseEntity { // 다른 엔티티가 공유할 목적으로 생성
 
 }
