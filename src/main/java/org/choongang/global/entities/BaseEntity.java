f9da3c207fdc4129b55a93ca1de72a3a14244091
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:49:21 +0900

    BaseEntity

--- src/main/java/org/choongang/global/entities/BaseEntity.java
+++ src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @Getter @Setter
 @MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
+@EntityListeners(AuditingEntityListener.class) // Q 클래스 생성하는 역할
 public abstract class BaseEntity { // 다른 엔티티가 공유할 목적으로 생성
 
 }
