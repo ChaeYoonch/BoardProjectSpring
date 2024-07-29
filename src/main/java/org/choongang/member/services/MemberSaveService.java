18245557f431117742121c3dcd6c7102e6edb28a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 15:49:15 +0900

    MemberSaveService

--- src/main/java/org/choongang/member/services/MemberSaveService.java
+++ src/main/java/org/choongang/member/services/MemberSaveService.java
@@ -3,6 +3,7 @@ package org.choongang.member.services;
 import lombok.RequiredArgsConstructor;
 import org.choongang.member.repositories.AuthoritiesRepository;
 import org.choongang.member.repositories.MemberRepository;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -12,4 +13,5 @@ import org.springframework.transaction.annotation.Transactional;
 public class MemberSaveService {
     private final MemberRepository memberRepository;
     private final AuthoritiesRepository authoritiesRepository;
+    private final PasswordEncoder passwordEncoder;
 }
