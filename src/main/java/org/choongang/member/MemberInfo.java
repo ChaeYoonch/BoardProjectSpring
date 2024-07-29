2ce72a359a14448023d3903f205a23f73e3ec8a8
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 17:00:57 +0900

    MemberInfo

--- src/main/java/org/choongang/member/MemberInfo.java
+++ src/main/java/org/choongang/member/MemberInfo.java
@@ -1,12 +1,22 @@
 package org.choongang.member;
 
+import lombok.Builder;
+import lombok.Data;
+import org.choongang.member.entities.Member;
 import org.springframework.security.core.GrantedAuthority;
 import org.springframework.security.core.userdetails.UserDetails;
 
 import java.util.Collection;
 import java.util.List;
 
+@Data
+@Builder
 public class MemberInfo implements UserDetails {
+    /* 구현 객체 */
+    private String email;
+    private String password;
+    private Collection<? extends GrantedAuthority> authorities;
+    private Member member;
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
