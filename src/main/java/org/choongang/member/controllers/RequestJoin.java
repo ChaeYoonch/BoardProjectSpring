639bc3400fbc0cefd6a217590f1e4863583edfe9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 10:50:38 +0900

    RequestJoin : 회원가입

--- src/main/java/org/choongang/member/controllers/RequestJoin.java
+++ src/main/java/org/choongang/member/controllers/RequestJoin.java
@@ -1,13 +1,20 @@
 package org.choongang.member.controllers;
 
+import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
-public class RequestJoin {
-    // 사용자가 작성한 데이터 전달 - 커맨드 객체
+public class RequestJoin { // 사용자가 작성한 데이터 전달 - 커맨드 객체
+    @NotBlank // 필수 항목
     private String email; // 이메일
+
+    @NotBlank
     private String password; // 비밀번호
+
+    @NotBlank
     private String confirmPassword; // 비밀번호 확인
+
+    @NotBlank
     private String userName; // 회원명
     private String mobile; // 전화번호
     private boolean agree; // 약관 동의
