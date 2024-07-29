7239c5686f6fde8041d2ac6da5e7d96077acd95a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:49:52 +0900

    BaseEntity

--- src/main/java/org/choongang/global/entities/BaseEntity.java
+++ src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -6,9 +6,12 @@ import lombok.Getter;
 import lombok.Setter;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDateTime;
+
 @Getter @Setter
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class) // Q 클래스 생성하는 역할
 public abstract class BaseEntity { // 다른 엔티티가 공유할 목적으로 생성
-
+    private LocalDateTime createAt;
+    private LocalDateTime modifiedAt;
 }
