908f48561807d63b660d0842a870c25e1960d932
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 12:31:54 +0900

    JoinValidator

--- src/main/java/org/choongang/member/validators/JoinValidator.java
+++ src/main/java/org/choongang/member/validators/JoinValidator.java
@@ -25,5 +25,9 @@ public class JoinValidator implements Validator {
          * 3. 비밀번호 복잡성 체크 (알파벳 대/소문자, 숫자, 특수 문자 포함)
          * 4. 휴대전화번호 형식 체크
          */
+
+        RequestJoin form = (RequestJoin) target;
+
+        /* 2. 비밀번호 & 비밀번호 확인 일치 여부 체크 */
     }
 }
