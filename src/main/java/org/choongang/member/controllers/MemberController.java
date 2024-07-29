a5df59af88d6cc67cf6c55ff30ed592ecb1f76d5
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 10:56:30 +0900

    MemberController

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -1,5 +1,6 @@
 package org.choongang.member.controllers;
 
+import jakarta.validation.Valid;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,7 +16,7 @@ public class MemberController {
     }
 
     @PostMapping("/join")
-    public String joinPs() { // 회원가입 프로세스
+    public String joinPs(@Valid RequestJoin form) { // 회원가입 프로세스 | @Valid = 검증 | form 형태로 넘김 | 만들어지는 기준 = 앞의 RequestJoin 즉, 클래스명
         return "redirect:/member/login";
     }
 
