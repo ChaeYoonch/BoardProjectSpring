dacaa0ef2c5a5f0db2d413d14413bcc047b079e7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 14:51:43 +0900

    BaseEntity

--- src/main/java/org/choongang/global/entities/BaseEntity.java
+++ src/main/java/org/choongang/global/entities/BaseEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import java.time.LocalDateTime;
@@ -12,6 +14,11 @@ import java.time.LocalDateTime;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class) // Q 클래스 생성하는 역할
 public abstract class BaseEntity { // 다른 엔티티가 공유할 목적으로 생성
+    @CreatedDate
     private LocalDateTime createAt;
+
+    @LastModifiedDate
     private LocalDateTime modifiedAt;
+
+    private LocalDateTime deleteAt;
 }
