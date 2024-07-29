7c2416fba2dd6786875acbe65e56f044a8f50d41
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 15:38:18 +0900

    MemberRepository

--- src/main/java/org/choongang/member/repositories/MemberRepository.java
+++ src/main/java/org/choongang/member/repositories/MemberRepository.java
@@ -1,11 +1,14 @@
 package org.choongang.member.repositories;
 
 import org.choongang.member.entities.Member;
+import org.choongang.member.entities.QMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
 public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor {
-    default boolean exists(String email) {
+    default boolean exists(String email) { // 메서드 오버로드
+        QMember member = QMember.member;
 
+        return exists(member.email.eq(email));
     }
 }
