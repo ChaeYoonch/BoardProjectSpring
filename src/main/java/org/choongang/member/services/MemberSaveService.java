5a16901425a5d78cbaacffad6eb9f0f57b89c6f2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 15:55:54 +0900

    MemberSaveService

--- src/main/java/org/choongang/member/services/MemberSaveService.java
+++ src/main/java/org/choongang/member/services/MemberSaveService.java
@@ -1,6 +1,7 @@
 package org.choongang.member.services;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.controllers.RequestJoin;
 import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.AuthoritiesRepository;
 import org.choongang.member.repositories.MemberRepository;
@@ -15,6 +16,14 @@ public class MemberSaveService {
     private final MemberRepository memberRepository;
     private final AuthoritiesRepository authoritiesRepository;
     private final PasswordEncoder passwordEncoder;
+
+    /**
+     * 회원 가입 처리
+     * @param form
+     */
+    public void save(RequestJoin form) {
+
+    }
 
     public void save(Member member) {
 
