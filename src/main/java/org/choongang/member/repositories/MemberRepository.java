ab30a428289e0c16e3491262ff7076408aace861
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-29 15:51:20 +0900

    MemberRepository

--- src/main/java/org/choongang/member/repositories/MemberRepository.java
+++ src/main/java/org/choongang/member/repositories/MemberRepository.java
@@ -5,7 +5,12 @@ import org.choongang.member.entities.QMember;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.querydsl.QuerydslPredicateExecutor;
 
+import java.util.Optional;
+
 public interface MemberRepository extends JpaRepository<Member, Long>, QuerydslPredicateExecutor {
+
+    Optional<Member> findByEmail(String email);
+
     default boolean exists(String email) { // 메서드 오버로드
         QMember member = QMember.member;
 
