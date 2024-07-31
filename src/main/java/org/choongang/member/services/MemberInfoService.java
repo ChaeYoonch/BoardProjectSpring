79f017989ff5a6ae538116e2eae1599ac0244e98
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 09:23:16 +0900

    MemberInfoService

--- src/main/java/org/choongang/member/services/MemberInfoService.java
+++ src/main/java/org/choongang/member/services/MemberInfoService.java
@@ -1,9 +1,13 @@
 package org.choongang.member.services;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+@Service
+@RequiredArgsConstructor
 public class MemberInfoService implements UserDetailsService {
 
     @Override
