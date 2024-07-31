1d7bf54ab4cb1005b3947e199f3ee880bc8e1793
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 14:41:04 +0900

    QBoard

--- src/main/generated/org/choongang/board/entities/QBoard.java
+++ src/main/generated/org/choongang/board/entities/QBoard.java
@@ -1,4 +1,54 @@
 package org.choongang.board.entities;
 
-public class QBoard {
+import static com.querydsl.core.types.PathMetadataFactory.*;
+
+import com.querydsl.core.types.dsl.*;
+
+import com.querydsl.core.types.PathMetadata;
+import javax.annotation.processing.Generated;
+import com.querydsl.core.types.Path;
+
+/**
+ * QBoard is a Querydsl query type for Board
+ */
+@Generated("com.querydsl.codegen.DefaultEntitySerializer")
+public class QBoard extends EntityPathBase<Board> {
+
+    private static final long serialVersionUID = 1615681981L;
+
+    public static final QBoard board = new QBoard("board");
+
+    public final org.choongang.global.entities.QBaseMemberEntity _super = new org.choongang.global.entities.QBaseMemberEntity(this);
+
+    public final StringPath bid = createString("bid");
+
+    public final StringPath bname = createString("bname");
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> createAt = _super.createAt;
+
+    //inherited
+    public final StringPath createdBy = _super.createdBy;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> deleteAt = _super.deleteAt;
+
+    //inherited
+    public final DateTimePath<java.time.LocalDateTime> modifiedAt = _super.modifiedAt;
+
+    //inherited
+    public final StringPath modifiedBy = _super.modifiedBy;
+
+    public QBoard(String variable) {
+        super(Board.class, forVariable(variable));
+    }
+
+    public QBoard(Path<? extends Board> path) {
+        super(path.getType(), path.getMetadata());
+    }
+
+    public QBoard(PathMetadata metadata) {
+        super(Board.class, metadata);
+    }
+
 }
