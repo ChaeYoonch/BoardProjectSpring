c127807f8d1e1e4159466837f40ea42e03f57557
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 12:38:36 +0900

    MemberController

--- src/main/java/org/choongang/member/controllers/MemberController.java
+++ src/main/java/org/choongang/member/controllers/MemberController.java
@@ -76,9 +76,13 @@ public class MemberController {
     @ResponseBody @GetMapping("/test3")
     public void test3() {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication(); // setAuthentication : 로그인 상태 강제로 유지
-        MemberInfo memberInfo = (MemberInfo)authentication.getPrincipal();
 
         log.info("로그인 상태 : {}", authentication.isAuthenticated());
-        log.info("로그인 회원 : {}", memberInfo.toString());
+        if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof MemberInfo) { // 로그인 상태 - UserDetails 구현체 (getPrincipal)
+            MemberInfo memberInfo = (MemberInfo) authentication.getPrincipal();
+            log.info("로그인 회원 : {}", memberInfo.toString());
+        } else { // 미로그인 상태 - String / anonymousUser (getPrincipal())
+            log.info("getPrincipal() : {}", authentication.getPrincipal());
+        }
     }
 }
