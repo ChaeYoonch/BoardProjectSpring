9b86e4394afd13a54d8c6c9350b365d34d651581
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 14:36:52 +0900

    Board

--- src/main/java/org/choongang/board/entities/Board.java
+++ src/main/java/org/choongang/board/entities/Board.java
@@ -1,7 +1,16 @@
 package org.choongang.board.entities;
 
+import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.choongang.global.entities.BaseMemberEntity;
 
+@Data
+@Entity
+@Builder
+@NoArgsConstructor @AllArgsConstructor
 public class Board extends BaseMemberEntity { // 게시판 설정
 
 }
