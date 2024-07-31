41807abafd67d570b413153757beb9e7542f9c25
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 14:29:46 +0900

    BaseMemberEntity

--- src/main/java/org/choongang/global/entities/BaseMemberEntity.java
+++ src/main/java/org/choongang/global/entities/BaseMemberEntity.java
@@ -4,6 +4,8 @@ import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.LastModifiedBy;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
@@ -11,4 +13,9 @@ import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 @EntityListeners(AuditingEntityListener.class)
 public abstract class BaseMemberEntity extends BaseEntity { // 회원 관련만 작성
 
+    @CreatedBy
+    private String createdBy;
+
+    @LastModifiedBy
+    private String modifiedBy;
 }
