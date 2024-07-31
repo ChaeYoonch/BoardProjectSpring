5176555fa741dba6b454c7d36ac8a4a2cc80ee66
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 14:28:58 +0900

    BaseMemberEntity

--- src/main/java/org/choongang/global/entities/BaseMemberEntity.java
+++ src/main/java/org/choongang/global/entities/BaseMemberEntity.java
@@ -1,12 +1,14 @@
 package org.choongang.global.entities;
 
+import jakarta.persistence.EntityListeners;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 @MappedSuperclass
 @Getter @Setter
-
+@EntityListeners(AuditingEntityListener.class)
 public abstract class BaseMemberEntity extends BaseEntity { // 회원 관련만 작성
 
 }
