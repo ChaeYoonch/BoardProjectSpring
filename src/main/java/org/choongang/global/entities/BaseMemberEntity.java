a1a648faf5ea3b0ae3767d7ad57b8c9898da5cf1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 14:28:26 +0900

    BaseMemberEntity

--- src/main/java/org/choongang/global/entities/BaseMemberEntity.java
+++ src/main/java/org/choongang/global/entities/BaseMemberEntity.java
@@ -1,5 +1,12 @@
 package org.choongang.global.entities;
 
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+
+@MappedSuperclass
+@Getter @Setter
+
 public abstract class BaseMemberEntity extends BaseEntity { // 회원 관련만 작성
 
 }
