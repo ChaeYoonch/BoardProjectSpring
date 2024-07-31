d0c4fdd020863bee9a610575fc1351c6b90d8221
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 09:32:08 +0900

    MemberInfoService

--- src/main/java/org/choongang/member/services/MemberInfoService.java
+++ src/main/java/org/choongang/member/services/MemberInfoService.java
@@ -1,12 +1,17 @@
 package org.choongang.member.services;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.member.constants.Authority;
+import org.choongang.member.entities.Authorities;
 import org.choongang.member.entities.Member;
 import org.choongang.member.repositories.MemberRepository;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Objects;
 
 @Service
 @RequiredArgsConstructor
@@ -18,6 +23,9 @@ public class MemberInfoService implements UserDetailsService {
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
                                          // 여기 username 연동       이 예외 UsernameNotFoundException 연동
         Member member = memberRepository.findByEmail(username).orElseThrow(() -> new UsernameNotFoundException(username));
+
+        List<Authorities> tmp = Objects.requireNonNullElse(member.getAuthorities(), // 위의 member 연결
+                                List.of(Authorities.builder().member(member).authority(Authority.USER).build()));
 
         return null;
     }
