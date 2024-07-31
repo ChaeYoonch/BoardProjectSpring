485898ea205d26d47530ae2bf8bfad3ba5f9f474
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 09:50:38 +0900

    SecurityConfig

--- src/main/java/org/choongang/global/configs/SecurityConfig.java
+++ src/main/java/org/choongang/global/configs/SecurityConfig.java
@@ -14,7 +14,8 @@ public class SecurityConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception { // 반환값 = http | 스프링 시큐리티 주 설정 위치
         /* 로그인, 로그아웃 S */
         http.formLogin(f -> { // 영역 도메인별로 나눠서 설정 O -> 람다 형태
-            f.usernameParameter("email")
+            f.loginPage("/member/login") // 로그인 시 이동할 경로
+             .usernameParameter("email")
              .passwordParameter("password")
              .successForwardUrl("/") // 성공 시 이동할 경로
              .failureForwardUrl("/member/login?error=true"); // 실패 시 이동할 경로
