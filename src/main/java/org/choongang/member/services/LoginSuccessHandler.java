61e99469c751f81d5e3954a1d681fd902d99fd68
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 10:20:34 +0900

    LoginSuccessHandler

--- src/main/java/org/choongang/member/services/LoginSuccessHandler.java
+++ src/main/java/org/choongang/member/services/LoginSuccessHandler.java
@@ -1,5 +1,17 @@
 package org.choongang.member.services;
 
-public class LoginSuccessHandler { // 로그인 성공 시 이동 경로
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
 
+import java.io.IOException;
+
+public class LoginSuccessHandler implements AuthenticationSuccessHandler { // 로그인 성공 시 이동 경로
+
+    @Override
+    public void onAuthenticationSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+
+    }
 }
