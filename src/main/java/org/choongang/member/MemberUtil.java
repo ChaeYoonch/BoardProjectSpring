fd714ef307bd3032c767c4f7dd4e14f47a48734f
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 12:43:53 +0900

    MemberUtil : 편의 기능

--- src/main/java/org/choongang/member/MemberUtil.java
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -1,5 +1,16 @@
 package org.choongang.member;
 
+import org.choongang.member.entities.Member;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberUtil { // Member 편의 기능 추가 -> Security ContextHolder 사용
 
+    public boolean isLogin() { // 로그인 여부
+
+    }
+
+    public Member getMember() { // 의존성 주입으로 쉽게 사용 가능 -> 2차 가공
+
+    }
 }
