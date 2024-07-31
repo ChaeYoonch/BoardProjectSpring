a5a9e63f38e1bd9ab88c4a79067539ceef4da7b2
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 12:48:07 +0900

    MemberUtil : 편의 기능

--- src/main/java/org/choongang/member/MemberUtil.java
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -9,13 +9,15 @@ import org.springframework.stereotype.Component;
 public class MemberUtil { // Member 편의 기능 추가 -> SecurityContextHolder 사용
 
     public boolean isLogin() { // 로그인 여부
-
+        return getMember() != null;
     }
 
     public Member getMember() { // 의존성 주입으로 쉽게 사용 가능 -> 2차 가공 | MemberInfo X-> Member 사용
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof MemberInfo) { // 구현 객체 타입도 확인
+            MemberInfo memberInfo = (MemberInfo) authentication.getPrincipal(); // (MemberInfo) : 형변환
 
+            return memberInfo.getMember();
         }
 
         return null;
