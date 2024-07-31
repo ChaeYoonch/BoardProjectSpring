f24d2a56ea3afb0d5d3b2ec247d426d47020cb78
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 12:49:08 +0900

    MemberUtil : 편의 기능

--- src/main/java/org/choongang/member/MemberUtil.java
+++ src/main/java/org/choongang/member/MemberUtil.java
@@ -12,6 +12,14 @@ public class MemberUtil { // Member 편의 기능 추가 -> SecurityContextHolde
         return getMember() != null;
     }
 
+    public boolean isAdmin() {
+        if (isLogin()) {
+
+        }
+
+        return false;
+    }
+
     public Member getMember() { // 의존성 주입으로 쉽게 사용 가능 -> 2차 가공 | MemberInfo X-> Member 사용
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication.isAuthenticated() && authentication.getPrincipal() instanceof MemberInfo) { // 구현 객체 타입도 확인
@@ -19,7 +27,6 @@ public class MemberUtil { // Member 편의 기능 추가 -> SecurityContextHolde
 
             return memberInfo.getMember();
         }
-
         return null;
     }
 }
