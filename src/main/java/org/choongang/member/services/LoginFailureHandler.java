e68dcfdb7a0cd07d89b6a2388d0e5541ac7e1b89
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 10:00:50 +0900

    LoginFailureHandler

--- src/main/java/org/choongang/member/services/LoginFailureHandler.java
+++ src/main/java/org/choongang/member/services/LoginFailureHandler.java
@@ -1,5 +1,19 @@
 package org.choongang.member.services;
 
-public class LoginFailureHandler { // 로그인 실패 시 경로
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.AuthenticationFailureHandler;
+
+import java.io.IOException;
+
+public class LoginFailureHandler implements AuthenticationFailureHandler { // 로그인 실패 시 경로
+
+    @Override
+    // 로그인 실패시 메서드
+    public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception) throws IOException, ServletException {
+
+    }
 
 }
