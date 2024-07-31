f2bb761264b46c864905954ae6cb6bdf21232a22
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-07-31 10:55:04 +0900

    RequestLogin : 로그인 커맨드 객체

--- src/main/java/org/choongang/member/controllers/RequestLogin.java
+++ src/main/java/org/choongang/member/controllers/RequestLogin.java
@@ -12,4 +12,8 @@ public class RequestLogin { // 로그인 커맨드 객체 추가
     private String password;
 
     private boolean success = true;
+
+    private String code; // 에러 코드 -> 글로벌 에러로 출력될 수 있도록 만들 예정
+
+    private String defaultMessage;
 }
