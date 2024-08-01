c93b522b5fc24ac787432e4f4d27a43ea97d7b35
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 09:36:31 +0900

    MemberAuthenticationEntryPoint

--- src/main/java/org/choongang/member/services/MemberAuthenticationEntryPoint.java
+++ src/main/java/org/choongang/member/services/MemberAuthenticationEntryPoint.java
@@ -6,6 +6,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
+import javax.swing.*;
 import java.io.IOException;
 
 public class MemberAuthenticationEntryPoint implements AuthenticationEntryPoint {
@@ -17,5 +18,12 @@ public class MemberAuthenticationEntryPoint implements AuthenticationEntryPoint 
          * 회원 전용 페이지로 접근한 경우 - /mypage -> 로그인 페이지 이동
          * 관리자 페이지로 접근한 경우 - 응답 코드 401, 에러 페이지 출력
          */
+
+        String uri = request.getRequestURI(); // 주소로 알아봄
+        if (uri.contains("/admin")) { // 관리자 페이지
+            response.sendError(HttpServletResponse.SC_UNAUTHORIZED); // 응답 코드 401
+        } else { // 회원 전용 페이지
+
+        }
     }
 }
