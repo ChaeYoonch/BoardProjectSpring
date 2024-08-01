67004c06290ca91198f10030229c1852ac4a7fa7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 10:30:11 +0900

    CommonException

--- src/main/java/org/choongang/global/exceptions/CommonException.java
+++ src/main/java/org/choongang/global/exceptions/CommonException.java
@@ -1,5 +1,14 @@
 package org.choongang.global.exceptions;
 
-public class CommonException extends RuntimeException{
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
 
+import java.util.List;
+import java.util.Map;
+
+@Getter @Setter
+public class CommonException extends RuntimeException {
+    private HttpStatus status; // 조회 가능
+    private Map<String, List<String>> errorMessage; // 수정 가능
 }
