2541e2e733641d0b9e64abefe592b108770855e9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 11:17:19 +0900

    AlertRedirectException

--- src/main/java/org/choongang/global/exceptions/script/AlertRedirectException.java
+++ src/main/java/org/choongang/global/exceptions/script/AlertRedirectException.java
@@ -2,6 +2,7 @@ package org.choongang.global.exceptions.script;
 
 import lombok.Getter;
 import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
 
 @Getter // 조회용 메서드
 public class AlertRedirectException extends AlertException {
@@ -11,7 +12,14 @@ public class AlertRedirectException extends AlertException {
 
     public AlertRedirectException(String message, String url, HttpStatus status, String target) {
         super(message, status);
+
+        target = StringUtils.hasText(target) ? target : "self"; // target 있으면 target, 없으면 self (= 현재 창)
+
         this.url = url;
         this.target = target;
+    }
+
+    public AlertRedirectException(String message, String url, HttpStatus status) { // target 이 없는 상태 -> self 로 고정
+        this(message, url, status, null);
     }
 }
