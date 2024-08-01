db843168d9a34477072dde0a659bafeeb3f08948
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 11:15:01 +0900

    AlertRedirectException

--- src/main/java/org/choongang/global/exceptions/script/AlertRedirectException.java
+++ src/main/java/org/choongang/global/exceptions/script/AlertRedirectException.java
@@ -1,9 +1,15 @@
 package org.choongang.global.exceptions.script;
 
+import lombok.Getter;
 import org.springframework.http.HttpStatus;
 
+@Getter // 조회용 메서드
 public class AlertRedirectException extends AlertException {
-    public AlertRedirectException(String message, String url, HttpStatus status, String target) {
 
+    private String url;
+    private String target;
+
+    public AlertRedirectException(String message, String url, HttpStatus status, String target) {
+        super(message, status);
     }
 }
