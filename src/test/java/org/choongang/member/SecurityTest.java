f09529f888e97384642331ba66b2c840368eb42b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 14:18:15 +0900

    SecurityTest : 통합 테스트

--- src/test/java/org/choongang/member/SecurityTest.java
+++ src/test/java/org/choongang/member/SecurityTest.java
@@ -4,12 +4,13 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
 
 @SpringBootTest
 @ActiveProfiles("test") // 환경 변수 설정
@@ -28,7 +29,9 @@ public class SecurityTest {
     }
 
     @Test
-    void test2() {
-
+    @WithMockUser
+    void test2() throws Exception {
+        mockMvc.perform(get("/mypage"))
+               .andDo(print());
     }
 }
