fa8ff7965c804e336bee8bc5a18b5b6fa081faec
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 14:06:27 +0900

    SecurityTest

--- src/test/java/org/choongang/member/SecurityTest.java
+++ src/test/java/org/choongang/member/SecurityTest.java
@@ -1,8 +1,10 @@
 package org.choongang.member;
 
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class SecurityTest {
 
 }
