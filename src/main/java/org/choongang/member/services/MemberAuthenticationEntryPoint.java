bd2fadbb2870f8c125e7d1d4332553533a46a6c1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 09:33:54 +0900

    MemberAuthenticationEntryPoint

--- src/main/java/org/choongang/member/services/MemberAuthenticationEntryPoint.java
+++ src/main/java/org/choongang/member/services/MemberAuthenticationEntryPoint.java
@@ -12,6 +12,10 @@ public class MemberAuthenticationEntryPoint implements AuthenticationEntryPoint 
 
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response, AuthenticationException authException) throws IOException, ServletException {
-        System.out.println(authException);
+        // System.out.println(authException);
+        /**
+         * 회원 전용 페이지로 접근한 경우 - /mypage -> 로그인 페이지 이동
+         * 관리자 페이지로 접근한 경우 - 응답 코드 401, 에러 페이지 출력
+         */
     }
 }
