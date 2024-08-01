9ceed26c8c93907469f64488f936e24a953f1e32
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 11:13:59 +0900

    AlertRedirectException

--- src/main/java/org/choongang/global/exceptions/script/AlertRedirectException.java
+++ src/main/java/org/choongang/global/exceptions/script/AlertRedirectException.java
@@ -1,4 +1,9 @@
 package org.choongang.global.exceptions.script;
 
-public class AlertRedirectException {
+import org.springframework.http.HttpStatus;
+
+public class AlertRedirectException extends AlertException {
+    public AlertRedirectException(String message, String url, HttpStatus status, String target) {
+
+    }
 }
