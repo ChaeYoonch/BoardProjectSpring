49c384777207e7eea352df69991e2df8a7f2680c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 11:02:53 +0900

    AlertBackException

--- src/main/java/org/choongang/global/exceptions/script/AlertBackException.java
+++ src/main/java/org/choongang/global/exceptions/script/AlertBackException.java
@@ -1,5 +1,21 @@
 package org.choongang.global.exceptions.script;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.util.StringUtils;
+
 public class AlertBackException extends AlertException { // AlertException 하위
 
+    private String target;
+
+    public AlertBackException(String message, HttpStatus status, String target) {
+        super(message, status);
+
+        target = StringUtils.hasText(target) ? target : "self";
+
+        this.target = target;
+    }
+
+    public AlertBackException(String message, HttpStatus status) {
+        this(message, status, null);
+    }
 }
