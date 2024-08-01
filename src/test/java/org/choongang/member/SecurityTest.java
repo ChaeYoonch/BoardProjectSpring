6fb7b2cf4a225e8115a02d20a05b6853c0891fd7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 14:15:19 +0900

    SecurityTest : 통합 테스트

--- src/test/java/org/choongang/member/SecurityTest.java
+++ src/test/java/org/choongang/member/SecurityTest.java
@@ -4,9 +4,15 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+
 @SpringBootTest
+@ActiveProfiles("test") // 환경 변수 설정
 @AutoConfigureMockMvc
 public class SecurityTest {
 
@@ -14,7 +20,15 @@ public class SecurityTest {
     private MockMvc mockMvc;
 
     @Test
-    void test1() throws Exception{
+    void test1() throws Exception {
+        mockMvc.perform(post("/member/join")
+               .with(csrf().asHeader())
+               .param("email", "user01@test.org"))
+               .andDo(print());
+    }
+
+    @Test
+    void test2() {
 
     }
 }
