3c4b4be3fcac8b8b77fd6ac70269c22a420eb915
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 10:38:46 +0900

    ExceptionProcessor

--- src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
+++ src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
@@ -1,12 +1,13 @@
 package org.choongang.global.exceptions;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 public interface ExceptionProcessor {
     @ExceptionHandler(Exception.class)
-    default ModelAndView errorHandler(Exception e) { // 모든 에러 페이지 -> error.html 로 가도록 error.html 에 연동
+    default ModelAndView errorHandler(Exception e, HttpServletRequest request) { // 모든 에러 페이지 -> error.html 로 가도록 error.html 에 연동
 
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 기본 응답 코드 500 으로 설정 -> 필요시 교체할 수 있도록
         if (e instanceof CommonException commonException) { // commonException 얘로 형변환 해서 바로 사용할 수 있도록!
