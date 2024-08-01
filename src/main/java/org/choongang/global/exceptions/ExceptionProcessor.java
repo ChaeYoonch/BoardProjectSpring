79ed08388757774ccadc9faf152877d883722d5b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 10:34:26 +0900

    ExceptionProcessor

--- src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
+++ src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
@@ -1,10 +1,13 @@
 package org.choongang.global.exceptions;
 
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
 
 public interface ExceptionProcessor {
+    @ExceptionHandler(Exception.class)
+    default ModelAndView errorHandler(Exception e) {
+        ModelAndView mv = new ModelAndView(); // 반환값이 ModelAndView
 
-    default ModelAndView errorHandler() {
-
+        return mv;
     }
 }
