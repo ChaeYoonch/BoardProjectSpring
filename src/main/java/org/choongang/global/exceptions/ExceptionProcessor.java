d0a6e4b3690ef0a6089b9a44cff7bdcf2bd89d93
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 11:22:44 +0900

    ExceptionProcessor

--- src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
+++ src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
@@ -1,6 +1,7 @@
 package org.choongang.global.exceptions;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.choongang.global.exceptions.script.AlertException;
 import org.springframework.http.HttpStatus;
 import org.springframework.util.StringUtils;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,23 +11,30 @@ public interface ExceptionProcessor {
     @ExceptionHandler(Exception.class)
     default ModelAndView errorHandler(Exception e, HttpServletRequest request) { // 모든 에러 페이지 -> error.html 로 가도록 error.html 에 연동
 
+        ModelAndView mv = new ModelAndView(); // 반환값이 ModelAndView
         HttpStatus status = HttpStatus.INTERNAL_SERVER_ERROR; // 기본 응답 코드 500 으로 설정 -> 필요시 교체할 수 있도록
+        String tpl = "error/error"; // 기본 = 에러 페이지
         if (e instanceof CommonException commonException) { // commonException 얘로 형변환 해서 바로 사용할 수 있도록!
             status = commonException.getStatus();
+
+            // AlertRedirectException 연동
+            if (e instanceof AlertException) { // AlertException 의 하위 라면
+                tpl = "common/_execute_script"; // 자바 스크립트 형태의 Alert 메세지인 경우 common/_execute_script 로 이동함!
+                String script = String.format("alert('%s');", e.getMessage());
+            }
         }
 
         String url = request.getRequestURI();
         String qs = request.getQueryString();
 
         if (StringUtils.hasText(qs)) url += "?" + qs;
-
-        ModelAndView mv = new ModelAndView(); // 반환값이 ModelAndView
+        // 위 쪽의 ModelAndView mv = new ModelAndView(); 에서 mv 가져옴
         mv.addObject("message", e.getMessage());
         mv.addObject("status", status.value());
         mv.addObject("method", request.getMethod());
         mv.addObject("path", url);
         mv.setStatus(status); // 위의 status 연동한 것!
-        mv.setViewName("error/error");
+        mv.setViewName(tpl); // AlertRedirectException 연동된 tpl 가져옴
         return mv;
     }
 }
