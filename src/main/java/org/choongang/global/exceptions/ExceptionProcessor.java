b9e1b9b7c2c10a6e8f03b9893f41185a16ce42c5
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 10:34:58 +0900

    ExceptionProcessor

--- src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
+++ src/main/java/org/choongang/global/exceptions/ExceptionProcessor.java
@@ -5,7 +5,7 @@ import org.springframework.web.servlet.ModelAndView;
 
 public interface ExceptionProcessor {
     @ExceptionHandler(Exception.class)
-    default ModelAndView errorHandler(Exception e) {
+    default ModelAndView errorHandler(Exception e) { // 모든 에러 페이지 -> error.html 로 가도록 error.html 에 연동
         ModelAndView mv = new ModelAndView(); // 반환값이 ModelAndView
 
         return mv;
