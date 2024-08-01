92327da95f7bebf7fbd75574425d33571430f698
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 10:31:53 +0900

    CommonException

--- src/main/java/org/choongang/global/exceptions/CommonException.java
+++ src/main/java/org/choongang/global/exceptions/CommonException.java
@@ -12,6 +12,10 @@ public class CommonException extends RuntimeException {
     private HttpStatus status; // 조회 가능
     private Map<String, List<String>> errorMessage; // 수정 가능
 
+    public CommonException(String message) {
+        this(message, HttpStatus.INTERNAL_SERVER_ERROR); // 기본 응답 코드는 500 | 직접 설정한 오류 이쪽으로 !
+    }
+
     public CommonException(String message, HttpStatus status) {
         super(message);
         this.status = status;
