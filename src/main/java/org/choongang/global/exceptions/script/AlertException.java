d9021eaf247673b022528b1d0d53ca5f5809121c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 10:49:00 +0900

    AlertException

--- src/main/java/org/choongang/global/exceptions/script/AlertException.java
+++ src/main/java/org/choongang/global/exceptions/script/AlertException.java
@@ -1,4 +1,10 @@
 package org.choongang.global.exceptions.script;
 
-public class AlertException { // 자바 스크립트 형태로 메세지 출력
+import org.choongang.global.exceptions.CommonException;
+import org.springframework.http.HttpStatus;
+
+public class AlertException extends CommonException { // 자바 스크립트 형태로 메세지 출력
+    public AlertException(String message, HttpStatus status) {
+        super(message, status);
+    }
 }
