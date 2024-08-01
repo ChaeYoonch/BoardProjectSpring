e70006bad7808e1fbc940dc65369ff463705bc52
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 12:01:22 +0900

    CommonControllerAdvice

--- src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
+++ src/main/java/org/choongang/global/advices/CommonControllerAdvice.java
@@ -1,8 +1,12 @@
 package org.choongang.global.advices;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.member.MemberUtil;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 
+@RequiredArgsConstructor
 @ControllerAdvice("org.choongang")
 public class CommonControllerAdvice { // 모든 컨트롤러 공통 값 유지 (컨트롤러 실행 전에)
 
+    private final MemberUtil memberUtil;
 }
