dbf9d94650acb7cbcd5a3cc6ffcc1091e44d99ac
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-01 11:44:21 +0900

    MainController

--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -1,8 +1,9 @@
 package org.choongang.main.controllers;
 
+import org.choongang.global.exceptions.ExceptionProcessor;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class MainController { // 메인 페이지
+public class MainController implements ExceptionProcessor { // 메인 페이지
 
 }
