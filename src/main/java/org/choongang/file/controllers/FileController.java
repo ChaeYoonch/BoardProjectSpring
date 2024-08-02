04b9c978828fd13cef5d78fc902758556d303974
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 10:49:56 +0900

    FileController

--- src/main/java/org/choongang/file/controllers/FileController.java
+++ src/main/java/org/choongang/file/controllers/FileController.java
@@ -1,17 +1,20 @@
 package org.choongang.file.controllers;
 
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestPart;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.util.StringUtils;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/file")
 public class FileController { //REST 형태로 작업
 
     @PostMapping("/upload") // file 은 post 형태로 넘어오기 때문!
-    public void upload(@RequestPart("file") MultipartFile[] files) { // "file" 이 이름 | 1개는 낱개로 | 여러 개는 배열 형태로
+    public void upload(@RequestPart("file") MultipartFile[] files, // "file" 이 이름 | 1개는 낱개로 | 여러 개는 배열 형태로
+                       @RequestParam(name="gid", required = false) String gid,
+                       @RequestParam(name="location", required = false) String location) { // Multipart file 형태
 
+        gid = StringUtils.hasText(gid) ? gid : UUID.randomUUID().toString();
     }
 }
