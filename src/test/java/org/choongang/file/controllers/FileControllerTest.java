1e01c9cd76a3e96624ce73a03271dafba9cc23b6
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 11:53:58 +0900

    FileControllerTest

--- src/test/java/org/choongang/file/controllers/FileControllerTest.java
+++ src/test/java/org/choongang/file/controllers/FileControllerTest.java
@@ -1,6 +1,7 @@
 package org.choongang.file.controllers;
 
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -17,11 +18,18 @@ public class FileControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    private MultipartFile[] files;
+    private MultipartFile[] files; // * files
 
     @BeforeEach
     void init() {
         MockMultipartFile file1 = new MockMultipartFile("file", "test1.png", "images/png", "ABC".getBytes()); // MockMultipartFile -> 임시 파일 | Mock = 모의 객체
         MockMultipartFile file2 = new MockMultipartFile("file", "test2.png", "images/png", "DEF".getBytes());
+
+        files = new MultipartFile[] {file1, file2}; // * files 연동
+    }
+
+    @Test
+    void uploadTest() throws Exception {
+
     }
 }
