aa7feee0f9e854af78b48876eb189e38c9236e37
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 10:20:24 +0900

    FileUploadService

--- src/main/java/org/choongang/file/services/FileUploadService.java
+++ src/main/java/org/choongang/file/services/FileUploadService.java
@@ -1,5 +1,10 @@
 package org.choongang.file.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // 의존성 주입에 필수!!
 public class FileUploadService {
 
 }
