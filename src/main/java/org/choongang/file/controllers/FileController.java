18a3a4adbfeb9c3c34552cfbb0f53de0420404ea
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 11:50:25 +0900

    FileController

--- src/main/java/org/choongang/file/controllers/FileController.java
+++ src/main/java/org/choongang/file/controllers/FileController.java
@@ -1,15 +1,33 @@
 package org.choongang.file.controllers;
 
+import lombok.RequiredArgsConstructor;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.services.FileUploadService;
+import org.choongang.global.rests.JSONData;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/file")
+@RequiredArgsConstructor
 public class FileController { //REST 형태로 작업
 
+    private final FileUploadService uploadService;
+
     @PostMapping("/upload") // file 은 post 형태로 넘어오기 때문!
-    public void upload(@RequestPart("file") MultipartFile[] files, // "file" 이 이름 | 1개는 낱개로 | 여러 개는 배열 형태로
-                       @RequestParam(name="gid", required = false) String gid,
-                       @RequestParam(name="location", required = false) String location) { // Multipart file 형태
+    public ResponseEntity<JSONData> upload(@RequestPart("file") MultipartFile[] files, // "file" 이 이름 | 1개는 낱개로 | 여러 개는 배열 형태로
+                                           @RequestParam(name="gid", required = false) String gid,
+                                           @RequestParam(name="location", required = false) String location) { // Multipart file 형태
+
+        List<FileInfo> items = uploadService.upload(files, gid, location); // * items
+        HttpStatus status = HttpStatus.CREATED; // ** status
+        JSONData data = new JSONData(items); // * items 연동
+        data.setStatus(status); // ** status 연동
+
+        return ResponseEntity.status(status).body(data); // ** status 연동
     }
 }
