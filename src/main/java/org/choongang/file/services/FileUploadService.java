3a602a96063b87e6255a1e29b6e89fb4294ad5c9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 10:51:18 +0900

    FileUploadService

--- src/main/java/org/choongang/file/services/FileUploadService.java
+++ src/main/java/org/choongang/file/services/FileUploadService.java
@@ -2,9 +2,13 @@ package org.choongang.file.services;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 @Service
 @RequiredArgsConstructor // 의존성 주입에 필수!!
 public class FileUploadService {
 
+    public void upload(MultipartFile[] files, String gid, String location) {
+
+    }
 }
