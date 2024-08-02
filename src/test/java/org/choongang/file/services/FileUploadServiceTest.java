5136e6e3ef18397da3571cd8b1beb95c81bc6549
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 11:38:51 +0900

    FileUploadServiceTest

--- src/test/java/org/choongang/file/services/FileUploadServiceTest.java
+++ src/test/java/org/choongang/file/services/FileUploadServiceTest.java
@@ -1,10 +1,14 @@
 package org.choongang.file.services;
 
+import org.choongang.file.entities.FileInfo;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.util.List;
 
 @SpringBootTest
 @ActiveProfiles("test")
@@ -17,5 +21,9 @@ public class FileUploadServiceTest {
     void uploadTest() {
         MockMultipartFile file1 = new MockMultipartFile("file", "test1.png", "images/png", "ABC".getBytes()); // MockMultipartFile -> 임시 파일 | Mock = 모의 객체
         MockMultipartFile file2 = new MockMultipartFile("file", "test2.png", "images/png", "DEF".getBytes());
+
+        List<FileInfo> items = uploadService.upload(new MultipartFile[] { file1, file2}, "testgid", "testlocation");
+
+        items.forEach(System.out::println);
     }
 }
