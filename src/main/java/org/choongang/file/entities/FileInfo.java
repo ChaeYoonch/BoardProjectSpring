a3bc8ee6c68a012f1f38bc4e6d950ad46e0e8d66
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 09:53:02 +0900

    FileInfo

--- src/main/java/org/choongang/file/entities/FileInfo.java
+++ src/main/java/org/choongang/file/entities/FileInfo.java
@@ -1,10 +1,15 @@
 package org.choongang.file.entities;
 
 import jakarta.persistence.Entity;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Entity
+@Builder
+@NoArgsConstructor @AllArgsConstructor
 public class FileInfo {
 
 }
