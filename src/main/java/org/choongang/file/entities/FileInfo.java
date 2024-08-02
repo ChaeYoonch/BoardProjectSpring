c25e3fbd8341d7040168da45c6b515b4b3e77cc1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 10:13:43 +0900

    FileInfo

--- src/main/java/org/choongang/file/entities/FileInfo.java
+++ src/main/java/org/choongang/file/entities/FileInfo.java
@@ -10,6 +10,8 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.choongang.global.entities.BaseMemberEntity;
 
+import java.util.UUID;
+
 @Data
 @Entity
 @Builder
@@ -20,7 +22,7 @@ public class FileInfo extends BaseMemberEntity {
     private Long seq; // 서버에 업로드 될 파일 이름 : seq.확장자
 
     @Column(length = 45)
-    private String gid; // 그룹 ID
+    private String gid = UUID.randomUUID().toString(); // 그룹 ID
     private String location; // 그룹 안에 세부 위치
 
     private String fileName; // 파일명
