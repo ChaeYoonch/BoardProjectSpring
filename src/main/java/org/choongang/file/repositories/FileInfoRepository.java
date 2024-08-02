6bd8d43f3a6aacb0da18ec84b83f136e9219fbca
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 10:00:31 +0900

    FileInfoRepository

--- src/main/java/org/choongang/file/repositories/FileInfoRepository.java
+++ src/main/java/org/choongang/file/repositories/FileInfoRepository.java
@@ -1,5 +1,9 @@
 package org.choongang.file.repositories;
 
-public interface FileInfoRepository {
+import org.choongang.file.entities.FileInfo;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.querydsl.QuerydslPredicateExecutor;
+
+public interface FileInfoRepository extends JpaRepository<FileInfo, Long>, QuerydslPredicateExecutor<FileInfo> {
 
 }
