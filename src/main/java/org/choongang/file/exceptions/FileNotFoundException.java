39eba9e5d3f87aa35b9e1c2666a43918ea1ba385
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 12:29:51 +0900

    FileNotFoundException

--- src/main/java/org/choongang/file/exceptions/FileNotFoundException.java
+++ src/main/java/org/choongang/file/exceptions/FileNotFoundException.java
@@ -1,9 +1,11 @@
 package org.choongang.file.exceptions;
 
 import org.choongang.global.exceptions.script.AlertBackException;
+import org.springframework.http.HttpStatus;
 
 public class FileNotFoundException extends AlertBackException { // 메세지 띄우고 뒤로 back 함
     public FileNotFoundException() {
-
+        super("NotFound.file", HttpStatus.NOT_FOUND); // 응답 코드 404
+        setErrorCode(true); // CommonException 의 errorCode 연동
     }
 }
