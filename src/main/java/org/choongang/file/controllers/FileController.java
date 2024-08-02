02255396f98f909e8b9b0b6bfae3164e8d01d72b
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 10:43:30 +0900

    FileController

--- src/main/java/org/choongang/file/controllers/FileController.java
+++ src/main/java/org/choongang/file/controllers/FileController.java
@@ -2,6 +2,7 @@ package org.choongang.file.controllers;
 
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -10,7 +11,7 @@ import org.springframework.web.multipart.MultipartFile;
 public class FileController { //REST 형태로 작업
 
     @PostMapping("/upload") // file 은 post 형태로 넘어오기 때문!
-    public void upload(MultipartFile[] files) { // 1개는 낱개로 | 여러 개는 배열 형태로
+    public void upload(@RequestPart("file") MultipartFile[] files) { // "file" 이 이름 | 1개는 낱개로 | 여러 개는 배열 형태로
 
     }
 }
