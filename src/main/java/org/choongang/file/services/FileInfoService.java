1046409cec33f5284d87fab24a1c0fed72d5bc84
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 12:20:32 +0900

    FileInfoService

--- src/main/java/org/choongang/file/services/FileInfoService.java
+++ src/main/java/org/choongang/file/services/FileInfoService.java
@@ -1,11 +1,27 @@
 package org.choongang.file.services;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.repositories.FileInfoRepository;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class FileInfoService {
 
-    public
+    private final FileInfoRepository fileInfoRepository;
+
+    /**
+     * 파일 1개 조회
+     * @param seq : 파일 등록 번호
+     * @return
+     */
+    public FileInfo get(Long seq) {
+
+        return null;
+    }
+
+    public List<FileInfo> getList(String gid, String location) // gid - 그룹 ID 로 조회
 }
