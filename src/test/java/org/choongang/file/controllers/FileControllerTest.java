45d2e9d0d85646098ed4a1a8c82a51b58d49acf7
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 11:51:58 +0900

    FileControllerTest

--- src/test/java/org/choongang/file/controllers/FileControllerTest.java
+++ src/test/java/org/choongang/file/controllers/FileControllerTest.java
@@ -1,7 +1,25 @@
 package org.choongang.file.controllers;
 
-@SpringBootTest
+import org.junit.jupiter.api.BeforeEach;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.multipart.MultipartFile;
 
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
 public class FileControllerTest {
 
+    @Autowired
+    private MockMvc mockMvc;
+
+    private MultipartFile[] files;
+
+    @BeforeEach
+    void init() {
+
+    }
 }
