b0ed05711716db681037b6d51c68ab4025c02cda
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 11:36:43 +0900

    FileUploadServiceTest

--- src/test/java/org/choongang/file/services/FileUploadServiceTest.java
+++ src/test/java/org/choongang/file/services/FileUploadServiceTest.java
@@ -3,6 +3,7 @@ package org.choongang.file.services;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
@@ -14,6 +15,7 @@ public class FileUploadServiceTest {
 
     @Test
     void uploadTest() {
-        // MockMultipartFile -> 임시 파일 | Mock = 모의 객체
+        MockMultipartFile file1 = new MockMultipartFile("file", "test1.png", "images/png", "ABC".getBytes()); // MockMultipartFile -> 임시 파일 | Mock = 모의 객체
+        MockMultipartFile file2 = new MockMultipartFile("file", "test2.png", "images/png", "DEF".getBytes());
     }
 }
