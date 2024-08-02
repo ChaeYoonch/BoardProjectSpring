4a6ec56c605ece57d4c7ae250acd435bd7ef16b1
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 10:41:10 +0900

    FileController

--- src/main/java/org/choongang/file/controllers/FileController.java
+++ src/main/java/org/choongang/file/controllers/FileController.java
@@ -1,5 +1,10 @@
 package org.choongang.file.controllers;
 
-public class FileController {
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/file")
+public class FileController { //REST 형태로 작업
 
 }
