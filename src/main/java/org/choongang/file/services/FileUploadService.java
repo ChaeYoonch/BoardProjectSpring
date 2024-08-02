e772036a530956f7ddf4f8f7a3b059bd9c577cee
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-02 11:05:21 +0900

    FileUploadService

--- src/main/java/org/choongang/file/services/FileUploadService.java
+++ src/main/java/org/choongang/file/services/FileUploadService.java
@@ -1,6 +1,8 @@
 package org.choongang.file.services;
 
 import lombok.RequiredArgsConstructor;
+import org.choongang.file.entities.FileInfo;
+import org.choongang.file.repositories.FileInfoRepository;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -10,6 +12,8 @@ import java.util.UUID;
 @Service
 @RequiredArgsConstructor // 의존성 주입에 필수!!
 public class FileUploadService {
+
+    private final FileInfoRepository fileInfoRepository;
 
     public void upload(MultipartFile[] files, String gid, String location) {
         /**
@@ -21,10 +25,21 @@ public class FileUploadService {
 
         gid = StringUtils.hasText(gid) ? gid : UUID.randomUUID().toString();
 
-        /* 1. 파일 정보 저장 */
+        /* 1. 파일 정보 저장 */ // image.abc.def.png -> 가장 마지막 . 찾아 확장자 확인
         for (MultipartFile file : files) {
             String fileName = file.getOriginalFilename(); // 업로드 파일 원래 이름
             String contentType = file.getContentType(); // 파일 형식
+            String extension = fileName.substring(fileName.lastIndexOf(".")); // 확장자 확인
+
+            FileInfo fileInfo = FileInfo.builder() // 위의 String 뒤의 값 연동한 것
+                                        .gid(gid)
+                                        .location(location)
+                                        .fileName(fileName)
+                                        .contentType(contentType)
+                                        .extension(extension)
+                                        .build();
+
+            fileInfoRepository.saveAndFlush(fileInfo); // 위의 fileInfo 에 담은 값들 가져옴
         }
     }
 }
