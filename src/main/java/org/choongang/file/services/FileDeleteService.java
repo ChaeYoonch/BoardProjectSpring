a9056d932f0d93417ac057c53affc4a474015d32
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-06 11:24:00 +0900

    FileDeleteService

--- src/main/java/org/choongang/file/services/FileDeleteService.java
+++ src/main/java/org/choongang/file/services/FileDeleteService.java
@@ -2,6 +2,7 @@ package org.choongang.file.services;
 
 import lombok.RequiredArgsConstructor;
 import org.choongang.file.repositories.FileInfoRepository;
+import org.choongang.member.MemberUtil;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -9,4 +10,5 @@ import org.springframework.stereotype.Service;
 public class FileDeleteService {
     private final FileInfoService infoService; // 파일 삭제할 때 필요
     private final FileInfoRepository infoRepository;
+    private final MemberUtil memberUtil; // 얘로 체크 가능!
 }
