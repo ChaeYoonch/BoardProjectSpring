a4149124009f405df7ac1295f01d3edbbff2ca9a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-06 12:25:45 +0900

    MainController

--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -11,7 +11,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController implements ExceptionProcessor { // 메인 페이지
 
     @GetMapping
-    public String index(Model model) { // (속성)에 Model model 추가
+    public String index(Model model) { // (속성)에 Model model 추가 -> 추가된 값이 에 추가됨 (범위 바꿀 수 있는 기능이 연계되어 있음)
+
+        model.addAttribute("addCommonScript", "fileManager");
 
         return "front/main/index"; // 주소 X | template 경로
     }
