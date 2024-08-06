4fe241d5f079cc5fd45f0e7d08c8211882d29443
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-06 11:20:27 +0900

    FileDownloadService

--- src/main/java/org/choongang/file/services/FileDeleteService.java
+++ src/main/java/org/choongang/file/services/FileDeleteService.java
@@ -1,5 +1,10 @@
 package org.choongang.file.services;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor // 파일 정보 가져오는 데 필요함!
 public class FileDeleteService {
 
 }
