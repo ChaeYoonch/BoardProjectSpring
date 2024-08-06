a9fdf3073b2b708c8879c606c4c96440f57b9a8c
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-06 11:21:01 +0900

    FileDeleteService

--- src/main/java/org/choongang/file/services/FileDeleteService.java
+++ src/main/java/org/choongang/file/services/FileDeleteService.java
@@ -6,5 +6,6 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor // 파일 정보 가져오는 데 필요함!
 public class FileDeleteService {
+    private final FileInfoService infoService; // 파일 삭제할 때 필요
 
 }
