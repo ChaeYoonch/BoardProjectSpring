06754afe25bf83bfa05e74af09f2da5e69f27403
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-06 12:23:27 +0900

    MainController

--- src/main/java/org/choongang/main/controllers/MainController.java
+++ src/main/java/org/choongang/main/controllers/MainController.java
@@ -2,6 +2,7 @@ package org.choongang.main.controllers;
 
 import org.choongang.global.exceptions.ExceptionProcessor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -10,7 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class MainController implements ExceptionProcessor { // 메인 페이지
 
     @GetMapping
-    public String index() {
+    public String index(Model model) { // (속성)에 Model model 추가
 
         return "front/main/index"; // 주소 X | template 경로
     }
