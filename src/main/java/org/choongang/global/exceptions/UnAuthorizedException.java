0d27b11668eca3afe30f40fa5e04b7900c7e6366
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-06 11:32:13 +0900

    UnAuthorizedException

--- src/main/java/org/choongang/global/exceptions/UnAuthorizedException.java
+++ src/main/java/org/choongang/global/exceptions/UnAuthorizedException.java
@@ -6,4 +6,9 @@ public class UnAuthorizedException extends CommonException{
     public UnAuthorizedException(String message) {
         super(message, HttpStatus.UNAUTHORIZED);
     }
+
+    public UnAuthorizedException() {
+        this("UnAuthorized");
+        setErrorCode(true);
+    }
 }
