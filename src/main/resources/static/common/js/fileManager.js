b7527242d532664cc7b3a49139b166192e91191a
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-06 12:37:01 +0900

    fileManager.js

--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -31,6 +31,7 @@ window.addEventListener("DOMContentLoaded", function () {
     /* 파일 업로드 버튼 이벤트 처리 S */
     const fileuploads = document.getElementsByClassName("fileuploads");
     const fileEl = document.createElement("input");
+    fileEl.type = 'file'; // 위의 fileEl 값 연결
 
     for (const el of fileuploads) {
         el.addEventListener("click", function() {
