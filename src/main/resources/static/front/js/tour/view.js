1cfe42ac3bfa4df1730f0be963bcc9ab68275d7e
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 17:35:58 +0900

    view.js

--- src/main/resources/static/front/js/tour/view.js
+++ src/main/resources/static/front/js/tour/view.js
@@ -3,10 +3,16 @@ window.addEventListener("DOMContentLoaded", function() {
         center: {
             lat: 37.557756188912954,
             lng: 126.94062742683245,
-        }
+        },
+        marker: [
+            {lat: 37.557756188912954, lng: 126.94062742683245},
+            {lat: 37.557287959390024, lng: 126.94120499658828},
+            {lat: 37.561184514897825, lng: 126.94069261563956},
+        ],
     };
-
+    mapLib
+    /*
     mapLib.load("map1", 300, 300, options);
     mapLib.load("map2", 400, 400, options);
-    mapLib.load("map3", 500, 500, options);
+    mapLib.load("map3", 500, 500, options); */
 });
