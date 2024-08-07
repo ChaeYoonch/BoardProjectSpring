21bc26121c32b33d04ef30d3f5730f55357895ec
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 16:54:13 +0900

    map.js

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -4,8 +4,9 @@ const mapLib = {
      * @param mapId : 지도를_출력할_요소_Id_이름
      * @param width : 지도_너비
      * @param height : 지도_높이
+     * @param options : 옵션 / center: { lat: 위도, lng: 경도 ... }
      */
-     load(mapId, width= 300, height= 300) {
+     load(mapId, width= 300, height= 300, options) {
             const mapEl = document.getElementById(mapId);
             if (!mapEl) return;
 
