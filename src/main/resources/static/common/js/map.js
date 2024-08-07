b7dfc0b05baae203a32d86970c360c24c909b0ef
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 16:54:41 +0900

    map.js

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -4,11 +4,11 @@ const mapLib = {
      * @param mapId : 지도를_출력할_요소_Id_이름
      * @param width : 지도_너비
      * @param height : 지도_높이
-     * @param options : 옵션 / center: { lat: 위도, lng: 경도 ... }
+     * @param options : 옵션 / center: { lat: 위도, lng: 경도 ... } - 필수
      */
      load(mapId, width= 300, height= 300, options) {
             const mapEl = document.getElementById(mapId);
-            if (!mapEl) return;
+            if (!mapEl || !options?.center) return;
 
             mapEl.style.width = `${width}px`;
             mapEl.style.height = `${height}px`;
