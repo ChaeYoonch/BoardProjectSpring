00d8349a1e1435b806dadee7fd397d5abe8eee35
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 12:29:07 +0900

    common.js

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -1,7 +1,14 @@
 const commonLib = {
+    /**
+     *
+     * @param url
+     * @param method
+     * @param data
+     * @param headers
+     */
     ajaxLoad(url, method, data, headers) {
         if (!url) {
-
+            return;
         }
     }
 };
