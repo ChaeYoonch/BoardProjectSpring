e5608a6189fa47955274d58513209f2beb44aaed
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 14:06:08 +0900

    common.js

--- src/main/resources/static/common/js/common.js
+++ src/main/resources/static/common/js/common.js
@@ -1,10 +1,6 @@
 const commonLib = {
     /**
      * ajax 요청 공통
-     * @param url
-     * @param method
-     * @param data
-     * @param headers
      */
     ajaxLoad(url, method, data, headers) {
         if (!url) {
