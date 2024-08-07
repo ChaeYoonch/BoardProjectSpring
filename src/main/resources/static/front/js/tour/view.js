3628568f0e86e7c026b416f9db9bb0d91f2babad
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 15:58:39 +0900

    view.js

--- src/main/resources/static/front/js/tour/view.js
+++ src/main/resources/static/front/js/tour/view.js
@@ -1,3 +1,8 @@
+const items = [
+    [126.94062742683245, 37.557756188912954],
+    [126.94120499658828, 37.557287959390024]
+];
+
 window.addEventListener("DOMContentLoaded", function() {
     const mapEl = document.getElementById("map");
     mapEl.style.width = "1000px";
