d711b8bb1c358e2aa7ed22ca1e81d6eccb8ba0de
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 16:49:49 +0900

    map.js

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -1,5 +1,10 @@
 const mapLib = {
-
+    /**
+     * 지도 로드
+     * @param mapId : 지도를_출력할_요소_Id_이름
+     * @param width : 지도_너비
+     * @param height : 지도_높이
+     */
         load(mapId, width, height) {
 
         }
