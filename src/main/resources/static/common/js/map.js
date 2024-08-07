5ce7e23f721b722d4695401d7ee746b9f5f1d2a9
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 17:27:35 +0900

    map.js

--- src/main/resources/static/common/js/map.js
+++ src/main/resources/static/common/js/map.js
@@ -15,7 +15,7 @@ const mapLib = {
 
         /* 지도 가운데 좌표 처리 S */
         const { center } = options;
-        const zoom = options?.zoom ?? 3; // 기본값 = 3
+        const zoom = options?.zoom ?? 3; // 기본값 = 3 | optional chaing 옵셔널 체이닝 연산자
         const position = new kakao.maps.LatLng(center.lat, center.lng);
         const map = new kakao.maps.Map(mapEl, {
             center: position,
