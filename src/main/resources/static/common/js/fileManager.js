352cd95625fffee0a2453f71e0ef2f587014a645
DESKTOP-GKGCP05\jungc <jungcy0401@naver.com>
2024-08-07 11:56:30 +0900

    fileManager.js

--- src/main/resources/static/common/js/fileManager.js
+++ src/main/resources/static/common/js/fileManager.js
@@ -23,7 +23,7 @@ const fileManager = {
                 formData.append("file", file);
             }
 
-            if ()
+            if (location && location.trim()) // 값이 있을 때만 설정
 
         } catch (e) {
             console.log(e);
